8605e551af9160970dcca54a30da57c7ad67bb54
nils <nilsxegger@gmail.com>
2021-03-27 13:43:45 +0100

    finished progress function, next step is to implement downloading

--- six_historical_prices_crawler/six_crawler.py
+++ six_historical_prices_crawler/six_crawler.py
@@ -1,9 +1,14 @@
 from sys import argv
+from enum import Enum
 import os
 import requests as req
 from datetime import datetime
 
 print_debug = True
+
+class DataType:
+    Monthly = 1
+    Daily = 2
 
 def debug(*args):
     global print_debug
@@ -22,26 +27,41 @@ def find_last_date(folder_list: list, file_prefix:str, file_len: int, date_start
             parsed_date = datetime.strptime(file_date, date_format)
             if last_date is None or parsed_date < last_date:
                 last_date = parsed_date
+    if last_date is None:
+        last_date = datetime.today()
     return last_date
 
 
-def get_progress(folder: str) -> datetime:
+def get_progress(folder: str):
     folder = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
     if len(folder) == 0:
         debug("Starting with empty directory.")
-        return datetime.today()
+        return (DataType.Monthly, datetime.today())
     else:
         folder.sort()
         last_month = find_last_date(folder, 'monthly-trade-data-', 29, 19, 25, '%Y%m')
         last_blue_chip = find_last_date(folder, 'swiss_blue_chip_shares_', 37, 23, 33, '%Y-%m-%d')
         last_small_and_mid = find_last_date(folder, 'mid_and_small_caps_swiss_shares_', 46, 32, 42, '%Y-%m-%d')
-        debug(last_month)
-        debug(last_blue_chip)
-        debug(last_small_and_mid)
+        if last_month < last_blue_chip and last_month < last_small_and_mid:
+            return (DataType.Monthly, last_month)
+        else:
+            return (DataType.Daily, last_blue_chip if last_blue_chip >= last_small_and_mid else last_small_and_mid)
 
 if __name__ == '__main__':
     if len(argv) < 2:
         print_help()
     else:
         folder = argv[1]
-        get_progress(folder)
+        data_type, date = get_progress(folder))
+        if data_type == DataType.Monthly:
+            # TODO download next month, if month data does not exist, download all days separately
+            pass
+        else:
+            # TODO download each day until first of month for both blue chip and small and mid, after downloading the first, try downloading the next monthly, if not, downlaod whole month seperately again
+            pass
+        
+
+
+
+
+
